74748ecbb1f3406e7f73a270c77ea459fd3fb6ed
Nguyễn Anh Vũ <nguyenanhvu2172000@gmail.com>
2026-06-29 23:52:26 +0700

    gen-binary-29-06

---
+++ next-generation/next-binary.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+
+int ok=0, n, b[25];
+void in(){
+	for(int i=1;i<=n;i++) cout<<b[i]<<" ";
+	cout<<endl;
+}
+
+void sinh(){
+	int i=0;
+	while(b[i]){
+		b[i]=0;i--;
+	}
+	if(i==0) ok=1;
+	else b[i]=1;
+}
+
+main(){
+	cin>>n;
+	while(!ok){
+		in();
+		sinh();
+	}
+}
